816e2c6e23a3f43b4a0cb1a15160b579434af4b8
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-07-26 21:38:11 -0300

    Create index.tsx

---
+++ app/src/providers/sidebarProvider/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { SidebarContext } from '@/contexts/Sidebar';
+import { ReactNode, useState } from 'react';
+
+export const SidebarProvider = ({ children }: { children: ReactNode }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <SidebarContext.Provider value={{ open, setOpen }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
