b6dfc53656ba28edf5ae3969a2d7289fb484c8be
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-07-26 21:46:20 -0300

    add  comments

--- app/src/app/(auth)/user/dashboard/page.tsx
+++ app/src/app/(auth)/user/dashboard/page.tsx
@@ -3,9 +3,13 @@
 import { useGeoLocation } from '@/hooks/geoLocation';
 
 export default function DashboardUserPage() {
+  // Aqui está dados que vão vir diretamente da ação do usuário sobre o mapa;
   const { latitudes, setLatitudes } = useGeoLocation();
   console.log(latitudes);
+
+  // Recebendo os dados e inserindo eles dentro do estado que está no contexto global;
   const handleClick = () => {
+    //Dados X e Y
     const latitudeX = Math.random();
     const latitudeY = Math.random();
 
