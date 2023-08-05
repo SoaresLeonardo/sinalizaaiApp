a033da469ca9280f57272ddd45fd1fe031c6b4e7
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-08-05 14:55:00 -0300

    Update ModalHeader.tsx

--- app/src/components/Modal/ModalHeader.tsx
+++ app/src/components/Modal/ModalHeader.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export const ModalHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <header className="text-zinc-900 p-4">
+    <header className="text-zinc-200 p-4">
       <div className=" flex items-center justify-between p-3">{children}</div>
     </header>
   );
