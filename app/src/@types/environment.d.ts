106583c9f31377c838a1e5f4aa3bd1df4ad91da9
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-08-05 14:54:15 -0300

    Update environment.d.ts

--- app/src/@types/environment.d.ts
+++ app/src/@types/environment.d.ts
@@ -6,7 +6,7 @@ declare global {
       NEXT_PUBLIC_API_BASE_URL: string; // URL base da API; examplo: https://localhost:3000/suaRota/login?
       NEXT_PUBLIC_API_SIGNIN_URL: string; // URL de login da API
       NEXT_PUBLIC_API_ABRIR_CHAMADO: string; // URL de abrir um novo chamado
-      NEXT_PUBLIC_API_LIST_CHAMADOS: string; // URL de listar os chamados
+      NEXT_PUBLIC_API_GET_CHAMADOS_URL: string; // URL de listar os chamados
       NEXT_PUBLIC_USER_TYPE: 'admin' | 'user';
     }
   }
