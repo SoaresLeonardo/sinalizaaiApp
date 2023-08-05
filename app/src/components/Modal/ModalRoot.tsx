750c605089e3e7cb91ae9bb9d722c72b7d3ef083
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-08-05 14:55:05 -0300

    Update ModalRoot.tsx

--- app/src/components/Modal/ModalRoot.tsx
+++ app/src/components/Modal/ModalRoot.tsx
@@ -11,7 +11,7 @@ export const ModalRoot = ({ children, isOpen }: Props) => {
       {isOpen && (
         <div className="fixed inset-0 z-10 bg-black bg-opacity-40 text-neutral-800">
           <div className="flex h-full w-full items-center justify-center">
-            <div className="absolute z-50 mx-auto my-0 flex h-80 w-[720px] flex-col overflow-hidden bg-white rounded-xl">
+            <div className="absolute z-50 mx-auto my-0 flex h-80 w-[720px] flex-col overflow-hidden bg-[#0f1622] rounded-xl border border-gray-800">
               {children}
             </div>
           </div>
